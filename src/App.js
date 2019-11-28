c62f5316499cb113c8eeba371d9b275239b85116
Noppawat Chotiwan <noppawatchotiwan@gmail.com>
2019-11-28 22:26:46 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  
+  const [state,setState]=useState([]);
+  useEffect(async()=>{
+      const fetch = await axios.get('https://movie-mb-api.herokuapp.com/home');
+      const data = fetch.data;
+      setState(data);
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
     </div>
   );
 }
