9f28304e2ce9ac59a75d6650886cec971b5e8f1c
Noppawat Chotiwan <noppawatchotiwan@gmail.com>
2019-11-29 18:18:27 +0700

    Change ui to masonary on home and add rating

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import uuid from 'uuid';
 import moment from 'moment';
 import '../styles/Home.css';
+import heart from '../media/love.png';
 
 const Home = ()=>{
 
@@ -19,25 +20,25 @@ const Home = ()=>{
     const renderMovieList = () =>{
         const movieThumnailDoamin = 'https://image.tmdb.org/t/p/w500';
         return(
-            <div className='row'>
-            <div className='card-deck '>
-                {state.map((idx)=>    
-                    <div className='col-4 mb-3' key={uuid()}>
-                        <div className="card hvr-float ">                 
-                            <img className="card-img-top" src={`${movieThumnailDoamin}${idx.poster_path}`} alt={idx.title}/>                     
-                                <div className="card-body">                               
-                                        <h5 className="card-title">{idx.title}</h5>
-                                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>                               
-                                </div>
-                                
-                                <div className="card-footer " style={{backgroundColor:'white'}}>                                   
-                                        <small className="text-muted">On cinema {moment(idx.release_date).format('LL')}</small>
-                                </div>                            
+            <div className='card-columns mt-4 '>
+                {state.map((idx)=>{
+                    const score = Math.floor(idx.vote_average/2);
+                    return(
+
+                        <div className=''>
+                            <div className="card  hvr-float  p-0">                 
+                                <img className="card-img-top" src={`${movieThumnailDoamin}${idx.poster_path}`} alt={idx.title}/>                     
+                                    <div className="card-body">                               
+                                            <h5 className="card-title">{idx.title}</h5>
+                                            <p className="card-text">{idx.overview}</p>   
+                                            {Array(score).fill(<img className='mr-1' src={heart} style={{width:'1rem'}}/>)}
+                                            <p className="card-text mt-1"><small className="text-muted">On cinema {moment(idx.release_date).format('LL')}</small></p>
+                                    </div>                                                
+                            </div>
                         </div>
-                    </div>     
-                    
-                        )}                    
-            </div>
+                        )
+                }
+                )}                    
             </div>
         )                    
     }
